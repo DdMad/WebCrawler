81693f11d647f6e37c8fd505d1cd4be6729e5fec
DdMad <ddmad1030@gmail.com>
2015-10-14 17:34:19 +0800

    Add a api method to set table cell.

--- CrawlerView.py
+++ CrawlerView.py
@@ -7,6 +7,8 @@ class App:
 
     def __init__(self, master, rows=6, columns=10):
 
+        self.root = master
+
         start = Button(master, text="Start", command=self.start)
         start.grid(row=0)
 
@@ -18,8 +20,13 @@ class App:
             content = Label(master, text=countries[row], borderwidth=1)
             content.grid(row=2+row, column=0, sticky="nsew", padx=1, pady=1)
 
+    def setTableCell(self, row, column, score):
+        cell = Label(self.root, text=score)
+        cell.grid(row=row, column=column, sticky="nsew", padx=1, pady=1)
+
     def start(self):
         print "Start Crawling!"
+        #self.setTableCell(4, 4, "test")
 
 root = Tk()
 root.title("Web Crawler")
