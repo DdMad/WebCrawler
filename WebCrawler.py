8a8e874e83b484043323d9d05c5c0f9e6ab99989
TrumpSqol <trumpsqol@gmail.com>
2015-10-15 14:34:54 +0800

    add IP address to country mapping & response time measurement

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,8 +1,10 @@
 import nltk
+import time
 import socket
 import urlparse
-from threading import Thread
 from bs4 import BeautifulSoup
+from ipwhois import IPWhois
+from threading import Thread
 from nltk.corpus import stopwords
 from readability.readability import Document
 
@@ -11,16 +13,18 @@ urlList = []
 visited = []
 relevant = []
 threadList = []
-THRESHOLD = 50
+THRESHOLD = 5
 
 # brand dictionary of a particular country
-country = {}
+brand_score = {}
 
 brandList = open("brands", "r").read().split(",")
 # read in all the keywords for judge a web site as relevant
 keywordList = open("keywords", "r").read().split(",")
 # open file contains base urls to begin crawl with, each country has one base url
 baseURLs = open("baseURLs", "r").read().split("\n")
+# get country name
+country = baseURLs.pop(0)
 
 
 # fetch html file for a given url
@@ -28,7 +32,7 @@ def geturl(url):
     url = urlparse.urlparse(url)
 
     host = url.hostname
-    ip_addr = socket.gethostbyname(host)
+    address = socket.gethostbyname(host)
     path = url.path
     if path == "":
         path = "/"
@@ -41,7 +45,9 @@ def geturl(url):
     # allow socket reuse
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    s.connect((ip_addr, port))
+    s.connect((address, port))
+    # measure response time
+    start_time = time.time()
     s.send("GET " + path + " HTTP/1.0\r\nHost: " + host + "\r\n\r\n")
 
     html = ""
@@ -50,42 +56,53 @@ def geturl(url):
         if data == "":
             break
         html += data
+    end_time = time.time()
+
+    response_time = end_time - start_time
 
     s.shutdown(socket.SHUT_RDWR)
     s.close()
 
-    return html
+    return [html, response_time]
 
 
 #  simple web crawler
-def crawler(country_dict):
+def crawler():
     while len(urlList) > 0 and len(relevant) < THRESHOLD:
         url = urlList.pop(0)
         if url not in visited:
             visited.append(url)
             try:
-                html = geturl(url)
-                # judge relevance of the html
-                is_relevant = processHtml(html, country_dict)
-                if is_relevant:
-                    relevant.append(url)
-                    # find all links in this html and append to urlList
-                    soup = BeautifulSoup(html, "html.parser")
-                    links = soup.find_all("a", href=True)
-                    for link in links:
-                        raw_link = urlparse.urlparse(link["href"])
-                        host = raw_link.hostname
-                        path = raw_link.path
-                        if host is None:
-                            host = urlparse.urlparse(url).hostname
-                        new_url = "http://" + host + path
-                        urlList.append(new_url)
-                    print(urlList)
+                ip_address = socket.gethostbyname(urlparse.urlparse(url).hostname)
+                ipw = IPWhois(ip_address)
+                region = ipw.lookup()['asn_country_code']
+                print region + ":" + str(url)
+                print "region to crawl: " + region
+                print(region == country)
+                if region == country:
+                    web_page = geturl(url)
+                    html = web_page[0]
+                    rtt = web_page[1]
+                    # judge relevance of the html
+                    is_relevant = processHtml(html)
+                    if is_relevant:
+                        relevant.append([url, rtt])
+                        # find all links in this html and append to urlList
+                        soup = BeautifulSoup(html, "html.parser")
+                        links = soup.find_all("a", href=True)
+                        for link in links:
+                            raw_link = urlparse.urlparse(link["href"])
+                            host = raw_link.hostname
+                            path = raw_link.path
+                            if host is None:
+                                host = urlparse.urlparse(url).hostname
+                            new_url = "http://" + host + path
+                            urlList.append(new_url)
             except:
-                print(url)
+                print "ERROR when dealing with: " + str(url)
 
 
-def processHtml(html, country_dict):
+def processHtml(html):
     html_body = html.split("\r\n\r\n")[1]
     # convert html to readable format
     article = Document(html_body).summary()
@@ -94,19 +111,19 @@ def processHtml(html, country_dict):
     # parse web page article
     soup = BeautifulSoup(article, "lxml")
     text = soup.text
-    print(title, text)
-    # do not consider small document
+    print "Title: " + title + "Content: " + text
+
+    # ignore small document
     if len(text) < 500:
         return False
 
-    # convert to lower case & tokenize web page brands
+    # convert to lower case & tokenize web page content
     tokens = nltk.word_tokenize(text.lower())
     # remove all stop words
     filtered = [w for w in tokens if w not in stopwords.words('english')]
 
-    # check keyword in title
+    # check keyword in the title
     is_title_relevant = False
-    print(title.lower())
     for word in brandList:
         if word in title.lower():
             is_title_relevant = True
@@ -122,7 +139,8 @@ def processHtml(html, country_dict):
             score += 10
         if word in keywordList:
             score += 5
-    print("score " + str(score))
+    print("Score: " + str(score))
+
     # assign weight to document based on score
     weight = 0
     if score < 100:
@@ -135,7 +153,7 @@ def processHtml(html, country_dict):
         weight = 400
     else:
         weight = 500
-    print("weight " + str(weight))
+    print("Page Relevance: " + str(weight))
 
     brand_dict = {}
     total_count = 0
@@ -153,23 +171,23 @@ def processHtml(html, country_dict):
         brand_dict[brand] = float(count)/total_count
 
     for brand, percentage in brand_dict.items():
-        if brand in country_dict:
-            country_dict[brand] += weight * percentage
+        if brand in brand_score:
+            brand_score[brand] += weight * percentage
         else:
-            country_dict[brand] = weight * percentage
+            brand_score[brand] = weight * percentage
 
     return True
 
 
 for baseURL in baseURLs:
     urlList.append(baseURL)
-    t = Thread(target=crawler, args=(country,))
+    t = Thread(target=crawler)
     t.start()
     threadList.append(t)
     print(len(threadList))
 
 for b in threadList:
     b.join()
-print(visited)
-print(relevant)
-print(country)
+print "Visited URLs:" + str(visited)
+print str(len(relevant)) + " Relevant URLs:" + str(relevant)
+print(brand_score)
