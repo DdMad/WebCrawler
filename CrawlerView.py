077286517f11e11f7cba504d69e80d7a9f507e40
DdMad <ddmad1030@gmail.com>
2015-10-17 14:59:18 +0800

    Update view.

--- CrawlerView.py
+++ CrawlerView.py
@@ -1,7 +1,11 @@
 from Tkinter import *
+import WebCrawler
 
-titles = ['Country', 'iPhone', 'Samsung', 'HTC', 'LG', 'Xiaomi', 'Huawei', 'Sony', 'Nexus', 'Lumia']
+titles = ['Country', 'iPhone', 'Samsung', 'Nexus', 'Lumia', 'HTC', 'Sony', 'LG', 'Xiaomi', 'Huawei']
 countries = ['UK', 'US', 'Singapore', 'India', 'Canada', 'Australia']
+
+brand = ['iphone', 'samsung', 'nexus', 'lumia', 'htc', 'sony', 'lg', 'xiaomi', 'huawei']
+url = ['ukurl', 'usurl', 'sgurl', 'inurl', 'caurl', 'auurl']
 
 class App:
 
@@ -10,7 +14,10 @@ class App:
         self.root = master
 
         start = Button(master, text="Start", command=self.start)
-        start.grid(row=0)
+        start.grid(row=0, column=0)
+
+        export = Button(master, text="Export", command=self.export)
+        export.grid(row=0, column=1)
 
         for column in range(columns):
             title = Label(master, text=titles[column], borderwidth=1)
@@ -27,10 +34,45 @@ class App:
     def start(self):
         print "Start Crawling!"
         #self.setTableCell(4, 4, "test")
+        # for country in range(6):
+        #     crawler = WebCrawler.WebCrawler(url[country])
+        #     crawler.crawl()
+        #     result = crawler.get_brand_score()
+        #
+        #     if result:
+        #         for bd in range(9):
+        #             if result[brand[bd]]:
+        #                 self.setTableCell(country+1, bd+1, result[brand[bd]])
+        #             else:
+        #                 self.setTableCell(country+1, bd+1, 0)
+        crawler = WebCrawler.WebCrawler("ukurl")
+        crawler.crawl()
+        result = crawler.get_brand_score()
+
+        print result
+
+        if result:
+            for bd in range(9):
+                if result[brand[bd]]:
+                    self.setTableCell(1, bd+1, result[brand[bd]])
+                else:
+                    self.setTableCell(1, bd+1, 0)
+
+
+    def export(self):
+        print "Export!"
 
 root = Tk()
 root.title("Web Crawler")
 
 app = App(root)
+
+# crawler = WebCrawler.WebCrawler("")
+# result = crawler.crawl()
+# print result["iphone"]
+
+
+
+
 
 root.mainloop()
