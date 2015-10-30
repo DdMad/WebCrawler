01a652afdf5e8ef0294a7e1ab3a8a17bc3031518
DdMad <ddmad1030@gmail.com>
2015-10-30 18:12:46 +0800

    Update

--- CrawlerView.py
+++ CrawlerView.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 import WebCrawler
+import csv
 
 titles = ['Country', 'iPhone', 'Samsung', 'Nexus', 'Lumia', 'HTC', 'Sony', 'LG', 'Xiaomi', 'Huawei']
 countries = ['UK', 'US', 'Singapore', 'India', 'Canada', 'Australia']
@@ -7,29 +8,34 @@ countries = ['UK', 'US', 'Singapore', 'India', 'Canada', 'Australia']
 brand = ['iphone', 'samsung', 'nexus', 'lumia', 'htc', 'sony', 'lg', 'xiaomi', 'huawei']
 url = ['ukurl', 'usurl', 'sgurl', 'inurl', 'caurl', 'auurl']
 
-
 class App:
 
     def __init__(self, master, rows=6, columns=10):
 
         self.root = master
 
+        master.minsize(width=700, height=320)
+        master.maxsize(width=700, height=320)
+
         start = Button(master, text="Start", command=self.start)
-        start.grid(row=0, column=0)
+        start.grid(row=9, column=0, pady=5, padx=5)
 
         export = Button(master, text="Export", command=self.export)
-        export.grid(row=0, column=1)
+        export.grid(row=9, column=1, pady=5, padx=5)
 
         clear = Button(master, text="Clear", command=self.clear)
-        clear.grid(row=0, column=2)
+        clear.grid(row=9, column=2, pady=5, padx=5)
+
+        exit = Button(master, text="Exit", command=master.quit)
+        exit.grid(row=9, column=3, pady=5, padx=5)
 
         for column in range(columns):
-            title = Label(master, text=titles[column], borderwidth=1)
-            title.grid(row=1, column=column, sticky="nsew", padx=1, pady=1)
+            title = Label(master, text=titles[column], borderwidth=1, font=("Helvetica", 15))
+            title.grid(row=0, column=column, sticky="nsew", padx=10, pady=5)
 
         for row in range(rows):
-            content = Label(master, text=countries[row], borderwidth=1)
-            content.grid(row=2+row, column=0, sticky="nsew", padx=1, pady=1)
+            content = Label(master, text=countries[row], borderwidth=1, font=("Helvetica", 15))
+            content.grid(row=2+row, column=0, sticky="nsew", padx=5, pady=10)
 
         self.clear()
 
@@ -39,10 +45,11 @@ class App:
 
     def start(self):
         print "Start Crawling!"
-        for country in range(4):
+        for country in range(6):
             print("######################### " + url[country] + " #########################")
             crawler = WebCrawler.WebCrawler(url[country])
             crawler.crawl()
+            crawler.write_files(countries[country])
             result = crawler.get_brand_score()
 
             if result:
@@ -67,6 +74,10 @@ class App:
     def export(self):
         print "Export!"
 
+        with open('test.csv', 'w', newline='') as fp:
+            a = csv.writer(fp, delimiter=',')
+            a.writerows()
+
     def clear(self):
         for country in range(6):
             for bd in range(9):
@@ -77,13 +88,5 @@ root = Tk()
 root.title("Web Crawler")
 
 app = App(root)
-
-# crawler = WebCrawler.WebCrawler("")
-# result = crawler.crawl()
-# print result["iphone"]
-
-
-
-
 
 root.mainloop()
